22cd4af79e4146aef7420974b7ce08f9b43b7af4
Sinoa <sinoa.dev@gmail.com>
2019-03-07 15:58:45 +0900

    単純なランタイムからのメモリ確保クラスを実装

--- Projects/SnowRabbit/VirtualMachine/Runtime/MemoryAllocator.cs
+++ Projects/SnowRabbit/VirtualMachine/Runtime/MemoryAllocator.cs
@@ -13,8 +13,11 @@
 // 2. Altered source versions must be plainly marked as such, and must not be misrepresented as being the original software.
 // 3. This notice may not be removed or altered from any source distribution.
 
+using System;
+
 namespace SnowRabbit.VirtualMachine.Runtime
 {
+    #region interface
     /// <summary>
     /// 仮想マシンが使用するメモリを確保するメモリアロケータインターフェイスです
     /// </summary>
@@ -26,6 +29,7 @@ namespace SnowRabbit.VirtualMachine.Runtime
         /// <param name="size">確保するメモリのサイズ</param>
         /// <param name="type">確保するメモリの利用タイプ</param>
         /// <returns>確保したメモリのブロックを返します</returns>
+        /// <exception cref="ArgumentOutOfRangeException">確保するサイズに 0 以下の指定は出来ません</exception>
         /// <exception cref="SrOutOfMemoryException">指定されたサイズのメモリを確保できませんでした</exception>
         MemoryBlock Allocate(int size, AllocationType type);
 
@@ -36,4 +40,101 @@ namespace SnowRabbit.VirtualMachine.Runtime
         /// <param name="memoryBlock">解放するメモリブロック</param>
         void Deallocate(MemoryBlock memoryBlock);
     }
+    #endregion
+
+
+
+    #region abstract MemoryAllocator
+    /// <summary>
+    /// 仮想マシンが使用するメモリを確保するメモリアロケータの抽象クラスです
+    /// </summary>
+    public abstract class MemoryAllocator : IMemoryAllocator
+    {
+        /// <summary>
+        /// メモリの確保をします。確保されるメモリのサイズは size が収まる様に確保されますが、実際の確保サイズは必ず一致しているかどうかを保証していません。
+        /// </summary>
+        /// <param name="size">確保するメモリのサイズ</param>
+        /// <param name="type">確保するメモリの利用タイプ</param>
+        /// <returns>確保したメモリのブロックを返します</returns>
+        /// <exception cref="ArgumentOutOfRangeException">確保するサイズに 0 以下の指定は出来ません</exception>
+        /// <exception cref="SrOutOfMemoryException">指定されたサイズのメモリを確保できませんでした</exception>
+        public abstract MemoryBlock Allocate(int size, AllocationType type);
+
+
+        /// <summary>
+        /// 指定されたメモリブロックを解放します。
+        /// </summary>
+        /// <param name="memoryBlock">解放するメモリブロック</param>
+        public abstract void Deallocate(MemoryBlock memoryBlock);
+
+
+        /// <summary>
+        /// 無効な確保サイズを要求された時に例外をスローします
+        /// </summary>
+        /// <param name="size">判断するための要求メモリサイズ</param>
+        /// <exception cref="ArgumentOutOfRangeException">確保するサイズに 0 以下の指定は出来ません</exception>
+        protected void ThrowExceptionIfRequestInvalidSize(int size)
+        {
+            // 0 以下のサイズ要求が有った場合は
+            if (size <= 0)
+            {
+                // 例外を吐く
+                throw new ArgumentOutOfRangeException(nameof(size), "確保するサイズに 0 以下の指定は出来ません");
+            }
+        }
+
+
+        /// <summary>
+        /// 要求メモリサイズの確保が出来ない例外をスローします
+        /// </summary>
+        protected void ThrowExceptionSrOutOfMemory()
+        {
+            // 無条件で例外をスローする
+            throw new SrOutOfMemoryException("指定されたサイズのメモリを確保できませんでした");
+        }
+    }
+    #endregion
+
+
+
+    #region Dynamic MemoryAllocator
+    /// <summary>
+    /// 直接.NETランタイムからメモリを割り当てるメモリアロケータクラスです
+    /// </summary>
+    public class DynamicManagedMemoryAllocator : MemoryAllocator
+    {
+        /// <summary>
+        /// メモリの確保をします。確保されるメモリのサイズは size が収まる様に確保されますが、実際の確保サイズは必ず一致しているかどうかを保証していません。
+        /// </summary>
+        /// <param name="size">確保するメモリのサイズ</param>
+        /// <param name="type">確保するメモリの利用タイプ</param>
+        /// <returns>確保したメモリのブロックを返します</returns>
+        /// <exception cref="ArgumentOutOfRangeException">確保するサイズに 0 以下の指定は出来ません</exception>
+        /// <exception cref="OutOfMemoryException">指定されたサイズのメモリを確保できませんでした</exception>
+        public override MemoryBlock Allocate(int size, AllocationType type)
+        {
+            // 事前の例外ハンドリングを行う
+            ThrowExceptionIfRequestInvalidSize(size);
+
+
+            // 要求サイズから確保するべき配列の要素数を求める
+            var allocateSize = size + ((8 - (size & 7)) & 7);
+            var allocateBlock = allocateSize >> 3;
+
+
+            // メモリブロックを生成後、必要配列数分確保して返す
+            return new MemoryBlock(new SrValue[allocateBlock], 0, allocateBlock);
+        }
+
+
+        /// <summary>
+        /// 指定されたメモリブロックを解放します。
+        /// </summary>
+        /// <param name="memoryBlock">解放するメモリブロック</param>
+        public override void Deallocate(MemoryBlock memoryBlock)
+        {
+            // .NETランタイムへ返却するだけなので、このメモリアロケータは何もしません
+        }
+    }
+    #endregion
 }
