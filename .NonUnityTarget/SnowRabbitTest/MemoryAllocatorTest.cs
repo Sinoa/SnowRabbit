1cb3921137e0030ffd71afc1372054f875073315
Sinoa <sinoa.dev@gmail.com>
2019-03-18 18:31:01 +0900

    標準アロケータのテストコード

--- .NonUnityTarget/SnowRabbitTest/MemoryAllocatorTest.cs
+++ .NonUnityTarget/SnowRabbitTest/MemoryAllocatorTest.cs
@@ -114,5 +114,47 @@ namespace SnowRabbit.Test
             // ダイナミックメモリ確保の場合は 8の倍数になって確保されているので要求サイズの8の倍数になっていることを確認する
             Assert.AreEqual(expectedLength, memoryBlock.Length);
         }
+
+
+        /// <summary>
+        /// StandardMemoryAllocator のメモリ確保テストを行います
+        /// </summary>
+        /// <param name="size">確保しようとしているメモリサイズ</param>
+        /// <param name="expectedLength">確保されたはずの配列の長さ（このテストケースではsize未満にならなければ良い）</param>
+        /// <param name="exceptionType">例外が発生する場合の例外タイプ、発生しない場合は null</param>
+        [TestCase(-1, 0, typeof(ArgumentOutOfRangeException))]
+        [TestCase(0, 0, typeof(ArgumentOutOfRangeException))]
+        [TestCase(1, 1, null)]
+        [TestCase(8, 1, null)]
+        [TestCase(9, 2, null)]
+        [TestCase(16, 2, null)]
+        [TestCase(100, 13, null)]
+        public void StandardAllocateTest(int size, int expectedLength, Type exceptionType)
+        {
+            // 2パターン分のインスタンスを生成する
+            var allocatorA = new StandardMemoryAllocator(1024);
+            var allocatorB = new StandardMemoryAllocator(new SrValue[1024]);
+            var memoryBlock = default(MemoryBlock);
+
+
+            // 例外タイプの指定が無いなら
+            if (exceptionType == null)
+            {
+                // メモリの確保は成功するテストを実行
+                Assert.DoesNotThrow(() => memoryBlock = allocatorA.Allocate(size, AllocationType.General));
+                Assert.DoesNotThrow(() => memoryBlock = allocatorB.Allocate(size, AllocationType.General));
+            }
+            else
+            {
+                // メモリ確保時に例外が発生するテストを実行して終了する（そもそも確保に失敗したら何も出来ない）
+                Assert.Throws(exceptionType, () => allocatorA.Allocate(size, AllocationType.General));
+                Assert.Throws(exceptionType, () => allocatorB.Allocate(size, AllocationType.General));
+                return;
+            }
+
+
+            // 通常、メモリアロケータは絶対的なサイズで確保することは無いので、希望サイズ以上の確保サイズならよしとする
+            Assert.GreaterOrEqual(expectedLength, memoryBlock.Length);
+        }
     }
 }
