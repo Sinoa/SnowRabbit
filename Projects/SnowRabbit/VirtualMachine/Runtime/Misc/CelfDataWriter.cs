2706437ebe0f9ce3a477389a3a3b9db39c7874fa
Sinoa <sinoa.dev@gmail.com>
2019-06-21 21:47:15 +0900

    [WIP]

--- Projects/SnowRabbit/VirtualMachine/Runtime/Misc/CelfDataWriter.cs
+++ Projects/SnowRabbit/VirtualMachine/Runtime/Misc/CelfDataWriter.cs
@@ -13,12 +13,82 @@
 // 2. Altered source versions must be plainly marked as such, and must not be misrepresented as being the original software.
 // 3. This notice may not be removed or altered from any source distribution.
 
+using System;
+using System.IO;
+
 namespace SnowRabbit.VirtualMachine.Runtime
 {
     /// <summary>
     /// Carrot Executable and Linkable Formatのデータを書き込むクラスです
     /// </summary>
-    public class CelfDataWriter
+    public class CelfDataWriter : IDisposable
     {
+        // メンバ変数定義
+        private bool disposed;
+        private StreamEndiannessControl endianControl;
+
+
+
+        #region Constructor and Dispose
+        /// <summary>
+        /// CelfDataWriter クラスのインスタンスを初期化します
+        /// </summary>
+        /// <param name="stream">CELFデータを書き込む対象となるストリーム</param>
+        /// <param name="leaveOpen">このインスタンスが破棄される時にストリームを開いたままにする場合は true を、閉じる場合は false</param>
+        /// <exception cref="ArgumentNullException">stream が null です</exception>
+        public CelfDataWriter(Stream stream, bool leaveOpen)
+        {
+            // リトルエンディアンコントロールのインスタンスを生成する
+            endianControl = new LittleEndianControl(stream, leaveOpen);
+        }
+
+
+        /// <summary>
+        /// インスタンスのリソースを解放します
+        /// </summary>
+        ~CelfDataWriter()
+        {
+            // ファイナライザからのDispose呼び出し
+            Dispose(false);
+        }
+
+
+        /// <summary>
+        /// インスタンスのリソースを解放します
+        /// </summary>
+        public void Dispose()
+        {
+            // DisposeからのDispose呼び出しをしてGCに自身のファイナライザを止めてもらう
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+
+        /// <summary>
+        /// インスタンスの実際のリソースを解放します
+        /// </summary>
+        /// <param name="disposing">マネージドを含む解放の場合は true を、アンマネージドのみの場合は false を指定</param>
+        protected virtual void Dispose(bool disposing)
+        {
+            // 既に解放済みなら
+            if (disposed)
+            {
+                // 何もしない
+                return;
+            }
+
+
+            // マネージの解放なら
+            if (disposing)
+            {
+                // エンディアンコントロールの解放
+                endianControl.Dispose();
+            }
+
+
+            // 解放済みマーク
+            disposed = true;
+        }
+        #endregion
     }
 }
---
+++ Projects/SnowRabbit/VirtualMachine/Runtime/Types/Process/CelfDataFormat.cs
@@ -0,0 +1,48 @@
+﻿// zlib/libpng License
+//
+// Copyright(c) 2019 Sinoa
+//
+// This software is provided 'as-is', without any express or implied warranty.
+// In no event will the authors be held liable for any damages arising from the use of this software.
+// Permission is granted to anyone to use this software for any purpose,
+// including commercial applications, and to alter it and redistribute it freely,
+// subject to the following restrictions:
+//
+// 1. The origin of this software must not be misrepresented; you must not claim that you wrote the original software.
+//    If you use this software in a product, an acknowledgment in the product documentation would be appreciated but is not required.
+// 2. Altered source versions must be plainly marked as such, and must not be misrepresented as being the original software.
+// 3. This notice may not be removed or altered from any source distribution.
+
+namespace SnowRabbit.VirtualMachine.Runtime
+{
+    /// <summary>
+    /// CELFヘッダの構造を表す構造体です
+    /// </summary>
+    public struct CelfHeader
+    {
+        public byte[] MagicSignature;
+        public ushort ObjectVersion;
+        public ushort RuntimeVersion;
+        public ushort CelfType;
+        public ushort CelfHeaderSize;
+        public uint SectionHeaderTableOffset;
+    }
+
+
+
+    public struct SectionHeaderTable
+    {
+        public ushort SectionHeaderCount;
+        public ushort ProgramCodeSectionIndex;
+        public ushort StringTableScetionIndex;
+        public ushort Reserved1;
+        public ulong Reserved2;
+        public SectionHeader[] SectionHeaders;
+    }
+
+
+
+    public struct SectionHeader
+    {
+    }
+}
