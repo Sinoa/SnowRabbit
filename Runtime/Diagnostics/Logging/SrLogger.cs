fee683332089b6c1ad754ccc238bb07d5b08c097
Sinoa <sinoa.dev@gmail.com>
2019-10-21 16:47:48 +0900

    トレースログフィルタ機能の実装

--- Runtime/Diagnostics/Logging/SrLogger.cs
+++ Runtime/Diagnostics/Logging/SrLogger.cs
@@ -14,6 +14,7 @@
 // 3. This notice may not be removed or altered from any source distribution.
 
 using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 
 namespace SnowRabbit.Diagnostics.Logging
@@ -25,6 +26,7 @@ namespace SnowRabbit.Diagnostics.Logging
     {
         // クラス変数宣言
         private static ISrLogHandler logHandler = SrConsoleLogHandler.Instance;
+        private static HashSet<string> traceTagFilterSet = new HashSet<string>();
 
 
 
@@ -34,16 +36,66 @@ namespace SnowRabbit.Diagnostics.Logging
         public static ISrLogHandler LogHandler { get => logHandler; set => logHandler = value ?? SrNullLogHandler.Instance; }
 
 
+        /// <summary>
+        /// トレースログをタグによるフィルタをするか否か。
+        /// </summary>
+        public static bool TraceTagFilter { get; set; }
+
+
+
+        /// <summary>
+        /// トレースログフィルタに出力を許可するタグを設定します。この関数は "TRACE" コンパイル定数を定義しない限り呼び出しは消去されます。
+        /// </summary>
+        /// <param name="traceTag">許可するタグ。null を渡された場合は、空文字列として扱われます。</param>
+        [Conditional("TRACE")]
+        public static void SetFilterTraceTag(string traceTag)
+        {
+            // フィルターセットに追加
+            traceTagFilterSet.Add(traceTag ?? string.Empty);
+        }
+
+
+        /// <summary>
+        /// トレースログフィルタに設定されたタグを外します。この関数は "TRACE" コンパイル定数を定義しない限り呼び出しは消去されます。
+        /// </summary>
+        /// <param name="traceTag">外すタグ。null を渡された場合は、空文字列として扱われます。</param>
+        [Conditional("TRACE")]
+        public static void UnsetFilterTraceTag(string traceTag)
+        {
+            // フィルターセットから削除
+            traceTagFilterSet.Remove(traceTag ?? string.Empty);
+        }
+
+
+        /// <summary>
+        /// トレースログフィルタに設定されたすべてのタグを削除します
+        /// </summary>
+        [Conditional("TRACE")]
+        public static void ClearFilterTraceTag()
+        {
+            // フィルターセットをクリア
+            traceTagFilterSet.Clear();
+        }
+
 
         /// <summary>
         /// トレースレベルのログを出力します。この関数は "TRACE" コンパイル定数を定義しない限り呼び出しは消去されます。
         /// ログを出力するためには "TRACE" コンパイル定数を定義してください。
+        /// また TraceTagFilter プロパティに true が設定されている場合 SetFilterTraceTag 関数によって設定されたタグ以外は出力されなくなります。
         /// </summary>
         /// <param name="tag">ログに紐付けるタグ。null を渡された場合は、空文字列として扱われます。</param>
         /// <param name="message">ログに出力するメッセージ。null を渡された場合は、空文字列として扱われます。</param>
         [Conditional("TRACE")]
         public static void Trace(string tag, string message)
         {
+            // トレースタグフィルタが有効 かつ トレースタグフィルタセットにタグが含まれていなければ
+            if (TraceTagFilter && !traceTagFilterSet.Contains(tag ?? string.Empty))
+            {
+                // ログの出力はしない
+                return;
+            }
+
+
             // ハンドラの対応する出力を行う
             logHandler.Trace(tag ?? string.Empty, message ?? string.Empty);
         }
