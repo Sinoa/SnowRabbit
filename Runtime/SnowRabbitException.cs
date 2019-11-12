3c8313db4237811b9fd126822bf766cd2a1283a9
Sinoa <sinoa.dev@gmail.com>
2019-11-12 23:01:47 +0900

    SnowRabbit の最基底例外クラスの実装

---
+++ Runtime/SnowRabbitException.cs
@@ -0,0 +1,63 @@
+﻿// zlib/libpng License
+//
+// Copyright(c) 2019 Sinoa
+//
+// This software is provided 'as-is', without any express or implied warranty.
+// In no event will the authors be held liable for any damages arising from the use of this software.
+// Permission is granted to anyone to use this software for any purpose,
+// including commercial applications, and to alter it and redistribute it freely,
+// subject to the following restrictions:
+//
+// 1. The origin of this software must not be misrepresented; you must not claim that you wrote the original software.
+//    If you use this software in a product, an acknowledgment in the product documentation would be appreciated but is not required.
+// 2. Altered source versions must be plainly marked as such, and must not be misrepresented as being the original software.
+// 3. This notice may not be removed or altered from any source distribution.
+
+using System;
+using System.Runtime.Serialization;
+
+namespace SnowRabbit
+{
+    /// <summary>
+    /// SnowRabbit が提供する例外の最も基底となる例外クラスです
+    /// </summary>
+    [Serializable]
+    public class SnowRabbitException : Exception
+    {
+        /// <summary>
+        /// SnowRabbitException クラスのインスタンスを初期化します
+        /// </summary>
+        public SnowRabbitException()
+        {
+        }
+
+
+        /// <summary>
+        /// SnowRabbitException クラスのインスタンスを初期化します
+        /// </summary>
+        /// <param name="message">例外に設定するメッセージ</param>
+        public SnowRabbitException(string message) : base(message)
+        {
+        }
+
+
+        /// <summary>
+        /// SnowRabbitException クラスのインスタンスを初期化します
+        /// </summary>
+        /// <param name="message">例外に設定するメッセージ</param>
+        /// <param name="inner">この例外を発生させる原因となった例外</param>
+        public SnowRabbitException(string message, Exception inner) : base(message, inner)
+        {
+        }
+
+
+        /// <summary>
+        /// シリアル化したデータを使用して SnowRabbitException クラスの新しいインスタンスを初期化します。
+        /// </summary>
+        /// <param name="info">スローされている例外に関するシリアル化済みオブジェクトデータを保持している SerializationInfo</param>
+        /// <param name="context">転送元または転送先についてのコンテキスト情報を含む StreamingContext</param>
+        protected SnowRabbitException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
