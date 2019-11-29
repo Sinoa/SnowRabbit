a553fc362fa5e2d1f6a8429797fedcffc16b3690
Sinoa <sinoa.dev@gmail.com>
2019-11-29 17:41:52 +0900

    解放の実装

--- Runtime/RuntimeEngine/VirtualMachine/SrvmMachine.cs
+++ Runtime/RuntimeEngine/VirtualMachine/SrvmMachine.cs
@@ -24,6 +24,7 @@ namespace SnowRabbit.RuntimeEngine.VirtualMachine
     public class SrvmMachine : SrDisposable
     {
         // 以下メンバ変数定義
+        private bool disposed;
         private readonly SrvmProcessor processor;
         private readonly SrvmMemory memory;
         private readonly SrvmFirmware firmware;
@@ -57,6 +58,49 @@ namespace SnowRabbit.RuntimeEngine.VirtualMachine
             firmware = factory.CreateFirmware() ?? throw new SrMachinePartsMissingException("Firmware マシンパーツを見失いました");
             storage = factory.CreateStorage() ?? throw new SrMachinePartsMissingException("Storage マシンパーツを見失いました");
             SrLogger.Trace(SharedString.LogTag.VIRTUAL_MACHINE, "Created SrvmMachine.");
+        }
+
+
+        /// <summary>
+        /// 確保したリソースを解放します。各種パーツのインスタンス解放も行います。
+        /// </summary>
+        /// <param name="disposing">マネージドを含む解放の場合は true を、アンマネージドのみ解放の場合は false を指定</param>
+        protected override void Dispose(bool disposing)
+        {
+            // 既に解放済みなら何もせず終了
+            if (disposed) return;
+            SrLogger.Trace(SharedString.LogTag.VIRTUAL_MACHINE, "Dispose SrvmMachine.");
+
+
+            // マネージドの解放なら
+            if (disposing)
+            {
+                // ストレージ、ファームウェア、メモリ、プロセッサ の順で解放する
+                storage.Dispose();
+                firmware.Dispose();
+                memory.Dispose();
+                processor.Dispose();
+            }
+
+
+            // 解放済みをマークして基底のDisposeも呼ぶ
+            disposed = true;
+            base.Dispose(disposing);
+        }
+
+
+        /// <summary>
+        /// 自身が既に解放済みの場合に例外をスローします
+        /// </summary>
+        /// <exception cref="ObjectDisposedException">既に解放済みです</exception>
+        private void ThrowExceptionIfObjectDisposed()
+        {
+            // 解放済みのマークが付いているなら
+            if (disposed)
+            {
+                // 解放済み例外を投げる
+                throw new ObjectDisposedException(null);
+            }
         }
     }
 }
