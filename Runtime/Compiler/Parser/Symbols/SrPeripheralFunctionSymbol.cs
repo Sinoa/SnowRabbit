05dae47fc5be4a14063e15968bc371c41768630e
Sinoa <sinoa.dev@gmail.com>
2020-01-06 20:57:38 +0900

    スクリプト関数、周辺機器関数のシンボルクラス定義

---
+++ Runtime/Compiler/Parser/Symbols/SrPeripheralFunctionSymbol.cs
@@ -0,0 +1,48 @@
+﻿// zlib/libpng License
+//
+// Copyright(c) 2019 Sinoa
+//
+// This software is provided 'as-is', without any express or implied warranty.
+// In no event will the authors be held liable for any damages arising from the use of this software.
+// Permission is granted to anyone to use this software for any purpose,
+// including commercial applications, and to alter it and redistribute it freely,
+// subject to the following restrictions:
+//
+// 1. The origin of this software must not be misrepresented; you must not claim that you wrote the original software.
+//    If you use this software in a product, an acknowledgment in the product documentation would be appreciated but is not required.
+// 2. Altered source versions must be plainly marked as such, and must not be misrepresented as being the original software.
+// 3. This notice may not be removed or altered from any source distribution.
+
+namespace SnowRabbit.Compiler.Parser.Symbols
+{
+    /// <summary>
+    /// 周辺機器関数シンボルを表す関数シンボルクラスです
+    /// </summary>
+    public class SrPeripheralFunctionSymbol : SrFunctionSymbol
+    {
+        // メンバ変数定義
+        private string mangledName;
+
+
+
+        /// <summary>
+        /// SrPeripheralFunctionSymbol クラスのインスタンスを初期化します
+        /// </summary>
+        /// <param name="name">シンボル名</param>
+        /// <param name="initialAddress">初期アドレス</param>
+        public SrPeripheralFunctionSymbol(string name, int initialAddress) : base(name, initialAddress)
+        {
+        }
+
+
+        /// <summary>
+        /// 周辺機器関数シンボル名をマングリングします
+        /// </summary>
+        /// <returns>マングリングした名前を返します</returns>
+        protected override string Mangling()
+        {
+            // マングリング済みならそのまま返して、まだなら周辺機器関数固有のマングリンク結果をそのまま返す
+            return mangledName ?? (mangledName = $"___SR_PF_{Name}___");
+        }
+    }
+}
