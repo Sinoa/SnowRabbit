e810c682c9d9fddab73e47e634ea8bf90034b730
Sinoa <sinoa.dev@gmail.com>
2020-01-15 19:18:46 +0900

    サンプル実行用の用意

--- .SampleApplication/ApplicationMain.cs
+++ .SampleApplication/ApplicationMain.cs
@@ -13,12 +13,18 @@
 // 2. Altered source versions must be plainly marked as such, and must not be misrepresented as being the original software.
 // 3. This notice may not be removed or altered from any source distribution.
 
+using System.IO;
+using SnowRabbit.Compiler;
+
 namespace SampleApplication
 {
     internal class ApplicationMain
     {
         private static void Main(string[] args)
         {
+            var outStream = new MemoryStream();
+            var compiler = new SrCompiler();
+            compiler.Compile("Sample.srs", outStream);
         }
     }
 }
