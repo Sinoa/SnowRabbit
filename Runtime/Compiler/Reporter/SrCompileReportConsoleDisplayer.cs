c9a5dc63f3afebe31d37f28fa28d5c126c86482e
Sinoa <sinoa.dev@gmail.com>
2020-01-20 18:10:56 +0900

    レポートは別途用意

---
+++ Runtime/Compiler/Reporter/SrCompileReportConsoleDisplayer.cs
@@ -0,0 +1,64 @@
+﻿// zlib/libpng License
+//
+// Copyright(c) 2020 Sinoa
+//
+// This software is provided 'as-is', without any express or implied warranty.
+// In no event will the authors be held liable for any damages arising from the use of this software.
+// Permission is granted to anyone to use this software for any purpose,
+// including commercial applications, and to alter it and redistribute it freely,
+// subject to the following restrictions:
+//
+// 1. The origin of this software must not be misrepresented; you must not claim that you wrote the original software.
+//    If you use this software in a product, an acknowledgment in the product documentation would be appreciated but is not required.
+// 2. Altered source versions must be plainly marked as such, and must not be misrepresented as being the original software.
+// 3. This notice may not be removed or altered from any source distribution.
+
+using System;
+using System.Collections.Generic;
+
+namespace Packages.SnowRabbit.Runtime.Compiler.Reporter
+{
+    /// <summary>
+    /// コンソールに対してコンパイルレポートを表示するクラスです
+    /// </summary>
+    public class SrCompileReportConsoleDisplayer : ISrCompileReportDisplayer
+    {
+        private static readonly Dictionary<CompileReportLevel, ConsoleColorData> colorTable;
+
+
+
+        static SrCompileReportConsoleDisplayer()
+        {
+            colorTable = new Dictionary<CompileReportLevel, ConsoleColorData>()
+            {
+                { CompileReportLevel.Error, new ConsoleColorData(ConsoleColor.Gray, ConsoleColor.Red) }
+            };
+        }
+
+
+        public void DisplayReport(CompileReport report)
+        {
+            var colorData = colorTable[report.Level];
+            Console.BackgroundColor = colorData.BackgroundColor;
+            Console.ForegroundColor = colorData.ForegroundColor;
+            Console.Write(report.Message);
+            Console.ResetColor();
+        }
+
+
+
+        private readonly struct ConsoleColorData
+        {
+            public readonly ConsoleColor BackgroundColor;
+            public readonly ConsoleColor ForegroundColor;
+
+
+
+            public ConsoleColorData(ConsoleColor background, ConsoleColor foreground)
+            {
+                BackgroundColor = background;
+                ForegroundColor = foreground;
+            }
+        }
+    }
+}
