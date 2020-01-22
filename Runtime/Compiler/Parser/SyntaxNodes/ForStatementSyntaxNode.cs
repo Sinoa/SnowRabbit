f91de5ca9288875df19afeba6f18d651849ccdfd
Sinoa <sinoa.dev@gmail.com>
2020-01-22 16:54:12 +0900

    [WIP]

--- Runtime/Compiler/Parser/SyntaxNodes/ForStatementSyntaxNode.cs
+++ Runtime/Compiler/Parser/SyntaxNodes/ForStatementSyntaxNode.cs
@@ -13,9 +13,40 @@
 // 2. Altered source versions must be plainly marked as such, and must not be misrepresented as being the original software.
 // 3. This notice may not be removed or altered from any source distribution.
 
+using SnowRabbit.RuntimeEngine;
+
 namespace SnowRabbit.Compiler.Parser.SyntaxNodes
 {
     public class ForStatementSyntaxNode : SyntaxNode
     {
+        public override void Compile(SrCompileContext context)
+        {
+            var initializeExpression = Children[0];
+            var conditionalExpression = Children[1];
+            var countingExpression = Children[2];
+            var instruction = new SrInstruction();
+
+
+            initializeExpression?.Compile(context);
+            instruction.Set(OpCode.Br);
+            context.AddBodyCode(instruction, false);
+
+            var forCountingHeadAddress = context.BodyCodeList.Count;
+            countingExpression?.Compile(context);
+
+
+            var forConditionalHeadAddress = context.BodyCodeList.Count;
+            conditionalExpression?.Compile(context);
+
+
+            for (int i = 3; i < Children.Count; ++i)
+            {
+                Children[i].Compile(context);
+            }
+
+
+            var 
+            var jumpOffsetAddress = 0;
+        }
     }
 }
