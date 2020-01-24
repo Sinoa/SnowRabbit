de36ee5082c4cd845061032b6733bdcdc99703ea
Sinoa <sinoa.dev@gmail.com>
2020-01-24 19:28:58 +0900

    ディスアセンブラも対応

--- Runtime/Compiler/Assembler/SrDisassembler.cs
+++ Runtime/Compiler/Assembler/SrDisassembler.cs
@@ -25,7 +25,7 @@ namespace SnowRabbit.Compiler.Assembler
 {
     public class SrDisassembler
     {
-        private static readonly string[] RegisterNameList = new string[] { "rax", "rbx", "rcx", "rdx", "rsi", "rdi", "rbp", "rsp", " r8", " r9", "r10", "r11", "r12", "r13", "r14", "r15", "[Reserved]", "[Reserved]", "[Reserved]", "[Reserved]", "[Reserved]", "[Reserved]", "[Reserved]", "[Reserved]", "[Reserved]", "[Reserved]", "[Reserved]", "[Reserved]", "[Reserved]", "[Reserved]", " ip", "zero" };
+        private static readonly string[] RegisterNameList = new string[] { "rax", "rbx", "rcx", "rdx", "rsi", "rdi", "rbp", "rsp", " r8", " r9", "r10", "r11", "r12", "r13", "r14", "r15", "r16", "r17", "r18", "r19", "r20", "r21", "r22", "r23", "r24", "r25", "r26", "r27", "r28", "r29", " ip", "zero" };
         private readonly Dictionary<int, string> labelTable = new Dictionary<int, string>();
         private readonly Dictionary<int, string> symbolTable = new Dictionary<int, string>();
 
